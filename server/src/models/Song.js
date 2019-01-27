18aafc37c399c0959ae6b875018bbb88ca22bc4f
LOKESH KUMAR YADAV <lokesh@cse.iitb.ac.in>
2019-01-27 15:30:06 +0530

    Songs model changed

--- server/src/models/Song.js
+++ server/src/models/Song.js
@@ -1,13 +1,32 @@
 module.exports = (sequelize, DataTypes)=> {
-	var table = sequelize.define("Song",{
-		songName: {
-			type: DataTypes.STRING,
-			unique: true
-		},
-		lyric: {
-			type: DataTypes.STRING
-		}
-	})
-	console.log("======= Songs table created =======")
-	return table
+    var table = sequelize.define("Song",{
+        songNameEng: {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        songNameHin: {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        Character: {
+            type: DataTypes.CHAR,
+        },
+        Artist: {
+            type: DataTypes.STRING,
+        },
+        Album: {
+            type: DataTypes.STRING,
+        },
+        Year: {
+            type: DataTypes.INTEGER,
+        },
+        Lyric: {
+            type: DataTypes.STRING
+        },
+        Link: {
+            type: DataTypes.STRING,
+        }
+    })
+    console.log("======= Songs table created =======")
+    return table
 }
