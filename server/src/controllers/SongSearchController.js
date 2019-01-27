ada49da67667bc67d5f20779acfda45761899893
LOKESH KUMAR YADAV <lokesh@cse.iitb.ac.in>
2019-01-27 22:50:05 +0530

    Added meaning to words ! 	-- creted JASON

--- server/src/controllers/SongSearchController.js
+++ server/src/controllers/SongSearchController.js
@@ -1,4 +1,5 @@
 const {Song} = require("../models")
+const {Dict} = require("../models")
 module.exports = {
 	// this module will perform the required functionality for 
 	// song search for do manipulation on song lyric
@@ -15,26 +16,64 @@ module.exports = {
 					songNameEng: req.body.songName
 				}
 			})
-			console.log(songs)
-			var arr = []
-			for (var id in songs) {
-			var results = songs[id].Lyric.match(/("[^"]+"|[^"\s]+)/g)
-			console.log(results)
-			res.send(results)
-			}
-			// var arr = []
-			// for (var id in songs) {
-			// 	console.log(songs[id].lyric)
-			// 	res.send(songs[id].lyric)
-			// }
+			// console.log(songs)
+			if (songs.length != 0){
+				
+				var arr = []
+				for (var id in songs) {
+				var results = songs[id].Lyric.match(/("[^"]+"|[^"\s]+)/g)
+				// console.log(results)
+				res.send(results)
+				}
 
-			
+				try{
+
+					var finalJson ="{ "+"meaning"+" :[ "
+					for( var n in results){
+						var word = await Dict.findAll({
+							limit : 1,
+							where: {
+								hword: results[n]
+							},
+							attributes: ["eword"]
+						})
+
+						if( word.length == 0){
+							finalJson = finalJson + " { null : "+results[n]+ " }, "
+						}else{
+
+						// console.log(word)
+						for (var idd in word) {
+							
+						// console.log(word[idd].eword)
+								finalJson = finalJson + " { "+ word[idd].eword+" : "+results[n]+ " }, "
+						
+						}
+					}
+						
+					}
+					finalJson  = finalJson + " ] }"
+					console.log(finalJson)
+
+				}catch(err){
+					console.log(err)
+				}
+				
+				// var arr = []
+				// for (var id in songs) {
+				// 	console.log(songs[id].lyric)
+				// 	res.send(songs[id].lyric)
+				// }
+
+			}else{
+				console.log("song not found in DB")
+			}	
 			//User.run("INSERT INTO User (songName) Values (\"abs\")")
 		}catch(err) {
 			// email already exits
 			console.log(err)
 			res.status(400).send( {
-				error : "error"
+				error : "Song not found in Database"
 			})
 		}
 	}
