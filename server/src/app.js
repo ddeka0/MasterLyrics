05a5b8c61d97bb9c9ab3654e38ba61a1d3495921
ddeka0 <dekadebashish3@gmail.com>
2019-01-27 14:45:42 +0530

    Updated with session management code
    status: sesison is created but not loaded next time

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,39 @@
 const express = require("express")
-
-const app = express()
-
+const session = require("express-session")
 const bodyParser = require("body-parser")
+const ONE_HOUR = 1000 * 60 * 60
+const {
+	NODE_ENV = "development",
+	SESS_NAME = "sid",
+	SESS_SECRET = "kjkjs&^*%&%^(*&(*9u43jkjkl897sd",
+	SESS_LIFETIME = ONE_HOUR
+} = process.env
+const IN_PROD = NODE_ENV === "production"
+const app = express()
+app.use(session({
+	name: SESS_NAME,
+	resave: true,
+	saveUninitialized: true,
+	secret: SESS_SECRET,
+	cookie: {
+		maxAge: SESS_LIFETIME,
+		sameSite: true,
+		secure: IN_PROD
+	} 
+}))
 const cors = require("cors")
 const morgan = require("morgan")
-
 const {sequelize} = require("./models")
 const config = require("./config/config")
 const routes = require("./routes")
 
 app.use(morgan("combined"))
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
 	extended: true
 }))
-app.use(bodyParser.json())
+
+
 //app.use(express.bodyParser())
 app.use(cors())
 
